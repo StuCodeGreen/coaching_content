607732bbc41bafbb6b8e790c0beaf92872ee2bde
stucodegreen <m.kuznecovas@gmail.com>
2020-05-16 21:05:05 +0100

    update

--- src/components/layout/card/Card.js
+++ src/components/layout/card/Card.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './styles';
 
-export const Card = ({ id, contentType, title, author }) => {
+export const Card = ({ id, contentType, title, author, image }) => {
   return (
     <div>
-      <h1>{id}</h1>
+      <img src={image.src} alt="banner" width="100" />
+
+      <div>
+        {' '}
+        {contentType.map((type) => (
+          <span className={`typeName ${type}`}>{type}</span>
+        ))}
+      </div>
+
+      <h1>{title}</h1>
+      <img src={author.image.src} alt={author.image.alt} width="100"></img>
+      <p>{author.name}</p>
     </div>
   );
 };
